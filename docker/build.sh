08ab79fec3c7d70341b620b578dc5b200e5a4226
Dung D. Phan <brianp.dung@utexas.edu>
2024-03-08 11:24:31 -0600

    Adding simDetectorIOC.

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+if [ ! -d "epics-base" ]; then
+    git clone --recursive https://github.com/epics-base/epics-base.git
+    perl -i -pe  's/#INSTALL_LOCATION=<fullpathname>/INSTALL_LOCATION=\/EPICS\/epics-base/g' epics-base/configure/CONFIG_SITE
+fi
+
+
+
 docker build -t ddp822/ut3rc:1.0.0 .
-docker push ddp822/ut3rc:1.0.0
+#docker push ddp822/ut3rc:1.0.0
